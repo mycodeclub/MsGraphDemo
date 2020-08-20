c441c213d59f54721710536baac427b6fdceb7da
MyCodeClub <ankitsahay1990@gmail.com>
2020-08-20 10:15:36 +0530

    Initial Commit
     - Working for email inbox and Calendar

---
+++ graph-tutorial/Controllers/EmailsController.cs
@@ -0,0 +1,19 @@
+﻿using graph_tutorial.Helpers;
+using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+
+namespace graph_tutorial.Controllers
+{
+    public class EmailsController : BaseController
+    {
+        // GET: Calendar
+        [Authorize]
+        public async Task<ActionResult> Index()
+        {
+            var emails = await GraphHelper.GetmessagesAsync();
+            return View(emails);
+        }
+    }
+}
